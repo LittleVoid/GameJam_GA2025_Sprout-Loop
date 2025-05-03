2956fa6d66ab5b0a0d3d520b447a939a3377b331
Tony <meis.laura92@googlemail.com>
2025-05-04 01:49:38 +0200

    AudioManager Setup
    -rough, untested

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,94 @@
+﻿//Responsible for this script: Tony Meis
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioManager audioManager;
+    private AudioSource backgroundPlayer, SFXPlayer;
+
+    [SerializeField] AudioClip MainMenu, Level, PauseMenu;
+    [SerializeField] List<AudioClip> Clicks;
+    [SerializeField] AudioClip Splash, Jump, Grow, Sprout, Whoosh, Victory, GameOver;
+
+    private void Awake()
+    {
+        if (audioManager != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+            Debug.Assert(audioManager != null, "NO UI FOUND!!!");
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        backgroundPlayer = transform.Find("BG_Player").GetComponent<AudioSource>();
+        SFXPlayer = transform.Find("SFX_Player").GetComponent<AudioSource>();
+
+        backgroundPlayer.clip = MainMenu;
+    }
+
+    public void SelectBackgroundMusic(string input)
+    {
+        switch (input)
+        {
+            case "Pause":
+                backgroundPlayer.clip = PauseMenu;
+                break;
+            case "MainMenu":
+                backgroundPlayer.clip = MainMenu;
+                break;
+            case "Level":
+                backgroundPlayer.clip = MainMenu;
+                break;
+            case "Win":
+                backgroundPlayer.Stop();
+                break;
+            case "Lose":
+                backgroundPlayer.Stop();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void PlaySFX(string input)
+    {
+        switch (input) 
+        {
+            case "Jump":
+                SFXPlayer.clip = Jump;
+                break;           
+            case "Splash":
+                SFXPlayer.clip = Splash;
+                break;   
+            case "Grow":
+                SFXPlayer.clip = Grow;
+                break;            
+            case "Sprout":
+                SFXPlayer.clip = Sprout;
+                break;            
+            case "Whoosh":
+                SFXPlayer.clip = Whoosh;
+                break;            
+            case "Victory":
+                SFXPlayer.clip = Victory;
+                break;            
+            case "GameOver":
+                SFXPlayer.clip = GameOver;
+                break;            
+            case "Click":
+                int rd = Random.Range(0, Clicks.Count);
+                SFXPlayer.clip = Clicks[rd];
+                break;            
+            default:
+                break;
+        }
+        SFXPlayer.Play();
+    }
+}
--- Assets/Scripts/UI_Scripts/UI_StartMenu.cs
+++ Assets/Scripts/UI_Scripts/UI_StartMenu.cs
@@ -7,8 +7,12 @@ public class UI_StartMenu : MonoBehaviour
 {
     GameObject Credit_Panel, QuitGame_Panel;
 
+    AudioManager audioManager;
+
     private void Awake()
     {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+
         Credit_Panel = transform.Find("Credit_Panel").gameObject;
         QuitGame_Panel = transform.Find("QuitGame_Panel").gameObject;
         Credit_Panel.SetActive(false);
@@ -17,30 +21,37 @@ public class UI_StartMenu : MonoBehaviour
 
     public void OpenCredits()
     {
+        audioManager.PlaySFX("Click");
         Credit_Panel.SetActive(true);
     }
 
     public void CloseCredits()
     {
+        audioManager.PlaySFX("Click");
         Credit_Panel.SetActive(false);
     }
 
     public void StartGame()
     {
+        audioManager.PlaySFX("Click");
         SceneManager.LoadScene(1);
+        audioManager.SelectBackgroundMusic("Level");
     }
 
     public void OpenQuitGameDialogue()
     {
+        audioManager.PlaySFX("Click");
         QuitGame_Panel.SetActive(true);
     }
 
     public void CloseQuitGameDialogue()
     {
+        audioManager.PlaySFX("Click");
         QuitGame_Panel.SetActive(false);
     }
     public void QuitGame()
     {
+        audioManager.PlaySFX("Click");
         Application.Quit();
     }
 }
