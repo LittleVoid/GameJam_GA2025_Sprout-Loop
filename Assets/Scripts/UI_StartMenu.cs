206d0768e33084f3dfda91e56c35c0baf6b8d3ee
Tony <meis.laura92@googlemail.com>
2025-05-03 20:46:44 +0200

    Start_Menu_Script
    -rough Setup
    -Buttons connected

---
+++ Assets/Scripts/UI_StartMenu.cs
@@ -0,0 +1,46 @@
+﻿//Responsible for this script: Tony Meis
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UI_StartMenu : MonoBehaviour
+{
+    GameObject Credit_Panel, QuitGame_Panel;
+
+    private void Awake()
+    {
+        Credit_Panel = transform.Find("Credit_Panel").gameObject;
+        QuitGame_Panel = transform.Find("QuitGame_Panel").gameObject;
+        Credit_Panel.SetActive(false);
+        QuitGame_Panel.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        Credit_Panel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        Credit_Panel.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenQuitGameDialogue()
+    {
+        QuitGame_Panel.SetActive(true);
+    }
+
+    public void CloseQuitGameDialogue()
+    {
+        QuitGame_Panel.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
