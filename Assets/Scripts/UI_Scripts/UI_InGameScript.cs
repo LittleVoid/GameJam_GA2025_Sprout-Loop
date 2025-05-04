b81a20438b261d94b9191b83fce69109c88a440e
Tony <meis.laura92@googlemail.com>
2025-05-04 17:55:03 +0200

    StartScene Adaption & Audio Setup
    Connecting Audio with scripts

--- Assets/Scripts/UI_Scripts/UI_InGameScript.cs
+++ Assets/Scripts/UI_Scripts/UI_InGameScript.cs
@@ -5,10 +5,13 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEditor.SearchService;
 
 public class UI_InGameScript : MonoBehaviour
 {
     UI_InGameScript inGameUI;
+    AudioManager audioManager;
+
     [SerializeField] TMP_Text Counter_Text, Header_Text, Subline_Text, ScoreValue_Text;
     [SerializeField] GameObject Pause_GameOver_Panel, Score_Panel, QuitGame_Panel;
     [SerializeField] Button Continue, NextLevel, TryAgain, Settings, MainMenu, Quit;
@@ -29,6 +32,9 @@ public class UI_InGameScript : MonoBehaviour
             Debug.Assert(inGameUI != null, "NO UI FOUND!!!");
         }
         DontDestroyOnLoad(this.gameObject);
+
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+
         /*
         Pause_GameOver_Panel = transform.Find("Pause_GameOver_Panel").gameObject;
         Score_Panel = Pause_GameOver_Panel.transform.Find("Notification_Panel/Score_Panel").gameObject;
@@ -77,6 +83,7 @@ public class UI_InGameScript : MonoBehaviour
         NextLevel.gameObject.SetActive(false);
         Header_Text.text = "Pause";
         Subline_Text.text = "Everyone needs a break uwu";
+        audioManager.SelectBackgroundMusic("Pause");
         Pause_GameOver_Panel.SetActive(true);
     }
 
@@ -84,6 +91,8 @@ public class UI_InGameScript : MonoBehaviour
     {
         Time.timeScale = 1.0f;
         Pause_GameOver_Panel.SetActive(false);
+        audioManager.SelectBackgroundMusic("Level");
+        audioManager.PlaySFX("Click");
     }
 
     public void OpenWinScreen(string input)
@@ -96,6 +105,8 @@ public class UI_InGameScript : MonoBehaviour
         //Score_Panel.gameObject.SetActive(true);
         Header_Text.text = "Wohooo! You won!";
         Subline_Text.text = "Onto the next challenge!";
+        audioManager.SelectBackgroundMusic("Win");
+        audioManager.PlaySFX("Victory");
         Pause_GameOver_Panel.SetActive(true);
     }
 
@@ -106,6 +117,8 @@ public class UI_InGameScript : MonoBehaviour
         Continue.gameObject.SetActive(false);
         Header_Text.text = input;
         Subline_Text.text = ":/ That did not go so well";
+        audioManager.SelectBackgroundMusic("Lose");
+        audioManager.PlaySFX("GameOver");
         Pause_GameOver_Panel.SetActive(true);
     }
 
@@ -123,31 +136,40 @@ public class UI_InGameScript : MonoBehaviour
 
     public void LoadNextLevel()
     {
+        audioManager.PlaySFX("Click");
         SceneManager.LoadScene(currentSceneIndex++);
+        audioManager.SelectBackgroundMusic("Level");
     }
 
     public void RestartLevel()
     {
+        audioManager.PlaySFX("Click");
         SceneManager.LoadScene(currentSceneIndex);
+        audioManager.SelectBackgroundMusic("Level");
     }
 
     public void ReturnToMainMenu()
     {
+        audioManager.PlaySFX("Click");
         SceneManager.LoadScene(0);
+        audioManager.SelectBackgroundMusic("MainMenu");
     }
 
 
     public void OpenQuitGameDialogue()
     {
+        audioManager.PlaySFX("Click");
         QuitGame_Panel.SetActive(true);
     }
 
     public void CloseQuitGameDialogue()
     {
+        audioManager.PlaySFX("Click");
         QuitGame_Panel.SetActive(false);
     }
     public void QuitGame()
     {
+        audioManager.PlaySFX("Click");
         Application.Quit();
     }
 }
--- Assets/Scripts/UI_Scripts/UI_StartMenu.cs
+++ Assets/Scripts/UI_Scripts/UI_StartMenu.cs
@@ -1,44 +1,58 @@
 ﻿//Responsible for this script: Tony Meis
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor.SearchService;
 
 
 public class UI_StartMenu : MonoBehaviour
 {
+    AudioManager audioManager;
+
     [SerializeField] GameObject Credit_Panel, QuitGame_Panel;
+
 
     private void Awake()
     {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+
         Credit_Panel.SetActive(false);
         QuitGame_Panel.SetActive(false);
+
+        audioManager.SelectBackgroundMusic("MainMenu");
     }
 
     public void OpenCredits()
     {
+        audioManager.PlaySFX("Click");
         Credit_Panel.SetActive(true);
     }
 
     public void CloseCredits()
     {
+        audioManager.PlaySFX("Click");
         Credit_Panel.SetActive(false);
     }
 
     public void StartGame()
     {
+        audioManager.SelectBackgroundMusic("Level");
         SceneManager.LoadScene(1);
     }
 
     public void OpenQuitGameDialogue()
     {
+        audioManager.PlaySFX("Click");
         QuitGame_Panel.SetActive(true);
     }
 
     public void CloseQuitGameDialogue()
     {
+        audioManager.PlaySFX("Click");
         QuitGame_Panel.SetActive(false);
     }
     public void QuitGame()
     {
+        audioManager.PlaySFX("Click");
         Debug.Log("quit");
         Application.Quit();
     }
